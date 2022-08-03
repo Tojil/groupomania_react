beb3d0ed756827d44b60e8180447fb4626615beb
Sergio Canto Sabido <sergio@hanullimparis.com>
2022-08-03 15:44:13 +0200

    Mise en place  du font Lato

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from '../src/assets/logo_secondaire.png';
+import logo from '../src/assets/icon_secondaire.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Groupomania </h1>
+        <h2>Soyez le bienvenue</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Groupomania the beginning.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Entrez
         </a>
       </header>
     </div>
